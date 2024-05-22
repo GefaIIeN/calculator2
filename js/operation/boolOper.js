042b8b93690a806522e1965220b2a2fe339c2138
Isaeva Marina <isaevamarina2020@gmail.com>
2024-05-22 15:58:26 +0300

    переработка кода

--- js/operation/boolOper.js
+++ js/operation/boolOper.js
@@ -7,16 +7,21 @@ function OperationBoolean() {
     this.getValues = () => {
         let values = [];
         values[0] = $("#value_5").val();
+        if (values[0] === 'false') {
+            values[0] = false;
+            return values
+            } else if (values[0] === 'true') {
+            values[0] = true;
+            return values
+            }
         return values;
     }
 
     this._checkValues = (values) => {
         const value0 = values[0]
-        if (value0 != '') {
-            if (value0 === 'false') {
-                this._values[0] = false;
-                } else if (value0 === 'true') {
-                this._values[0] = true;
+        if (value0 !== '') {
+            if ((value0 != false) || (value0 != true)) {
+                return false
                 }
             return true;
         }
