dc4cfa61e6b4d16ffe14e5f02c51210dbc5ea4fc
Isaeva Marina <isaevamarina2020@gmail.com>
2024-05-31 13:01:11 +0300

    реализован функционал операции сложение массивов

--- js/operation/plusArray.js
+++ js/operation/plusArray.js
@@ -3,55 +3,51 @@ function OperationPlusArray() {
 
     this._id = 'plusArray';
     this._title = 'Операция Сложение массивов';
+    this._values = [];
     this._values2 = [];
 
     this.getValues = () => {
+
         let values = [];
+        let arr1 = $("#value_3").val();
+        values = arr1.split(",");
         let values2 = [];
-        let arr1 = []
-        let arr2 = []
-        arr1 = $("#value_3")
-        arr2 = $("#value_4")
-        this._values = arr1.split(",");
-        this._values2 = arr2.split(",");
+        let arr2 = $("#value_4").val();
+        values2 = arr2.split(",");
 
-        this._values.filter(element => {
-            return parseInt(element)
-            })
-        this._values2.filter(element => {
-           return parseIntr(element)
-            })
-
-        return (values, values2)
+        return {
+            values: values, 
+            values2: values2
+        }
     }
 
 
-    this._checkValues = (values, values2) => {
-        const value0 = ''+values[0]
-        const value1 = ''+values2[0]
+    this._checkValues = () => {
+        
+        const value0 = this.getValues().values;
+        const value1 = this.getValues().values2;
 
-        if ((value0 != '') && (value1 != '')) {
-            const numValue = this._values.filter(element => {
-                return parseInt(element)
-            });
-            if (!numValue) {
-                return false;
-            }
+        console.log(value0, value1)
 
-            const numValue1 = this._values2.filter(element => {
-                return parseIntr(element);
-            });
-            if (!numValue1) {
-                return false;
+        if ((value0[0] != '') && (value1[0] != '')) {
+                const numValue = this.getValues().values.map(el=>parseInt(el))
+                if (numValue.includes(NaN)) {
+                    return false;
+                }
+
+                const numValue1 = this.getValues().values2.map(el=>parseInt(el))
+                if (numValue1.includes(NaN)) {
+                    return false;
             }
             return true;
         }
         return false;
     }
 
-    this.setValues = (values) => {
-        if (this._checkValues(values)) {
-            this._values = values;
+    this.setValues = () => {
+        if (this._checkValues()) {
+            this._values = this.getValues().values.map(Number);
+            this._values2 = this.getValues().values2.map(Number);
             return true;
         }
         return false;
@@ -60,8 +56,11 @@ function OperationPlusArray() {
     this.execute = () => {
 
             this.sum = [];
-            for (let i = 0; i < Math.max(this._values.length, this._values2.length); i++) {
-              sum.push ((this._values[i] || 0) + (this._values2[i] || 0)); 
+            const value0 = this._values;
+            const value1 = this._values2;
+
+            for (let i = 0; i < Math.max(value0.length, value1.length); i++) {
+                this.sum.push ((value0[i] || 0) + (value1[i] || 0)); 
             };
             return this.sum;
         }
